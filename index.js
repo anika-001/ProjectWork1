bef9ee2a5fc9783539c1292f9cf5bdbef5455396
anika-001 <dream.sweeter.30.09@gmail.com>
2022-01-09 18:39:34 +0530

    file optimisation

--- index.js
+++ index.js
@@ -68,3 +68,36 @@ for(let month of keys){
 // Each day check if any repayments are to be done today. Check if any loan is completed and to be deleted. Take x loans.
 // If tried to exceed K, ignore the loan, continue. 
 
+
+// Additionally, currently not required but for file upload optimisation and optimal read through we can use soomething like this too
+// var stream = fs.createReadStream(filePath, {flags: 'r', encoding: 'utf-8'});
+// var buf = '';
+
+// stream.on('data', function(d) {
+//     buf += d.toString(); // when data is read, stash it in a string buffer
+//     pump(); // then process the buffer
+// });
+
+// function pump() {
+//     var pos;
+
+//     while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+//         if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+//             buf = buf.slice(1); // discard it
+//             continue; // so that the next iteration will start with data
+//         }
+//         processLine(buf.slice(0,pos)); // hand off the line
+//         buf = buf.slice(pos+1); // and slice the processed data off the buffer
+//     }
+// }
+
+// function processLine(line) { // here's where we do something with a line
+
+//     if (line[line.length-1] == '\r') line=line.substr(0,line.length-1); // discard CR (0x0D)
+
+//     if (line.length > 0) { // ignore empty lines
+//         var obj = JSON.parse(line); // parse the JSON
+//         console.log(obj); // do something with the data here!
+//     }
+// }
+//file optimisation code end
